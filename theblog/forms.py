15e271b9c8c13995bccd57f80ba115d00c4378ff
Arun <intformbase@gmail.com>
2023-06-08 04:29:42 +0530

    added edit post feature

--- theblog/forms.py
+++ theblog/forms.py
@@ -12,3 +12,16 @@ class PostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Type your full post here'}),
         }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type your title here'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type your title tag here'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Type your full post here'}),
+        }
